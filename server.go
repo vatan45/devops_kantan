bbc1bd2614e1e0aef5b455af4edf46f434ba6e65
vatan malik <vmro45@gmail.com>
2024-10-04 23:29:28 +0530

    initial commits

---
+++ server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello-world", handleHelloWorld)
+	http.HandleFunc("/health", handleHealth)
+
+	addr := "localhost:8000"
+	log.Printf("Listening on %s ...", addr)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleHelloWorld(writer http.ResponseWriter, request *http.Request) {
+
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+
+	writeResponse(writer, "Hello world")
+
+}
+
+func handleHealth(writer http.ResponseWriter, request *http.Request) {
+
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+	writeResponse(writer, "Ok!")
+}
+
+func writeResponse(writer http.ResponseWriter, responseString string) {
+	response := []byte(responseString)
+	fmt.Println(response)
+	_, err := writer.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
